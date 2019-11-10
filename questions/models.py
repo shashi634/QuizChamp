8bb9ed390e4cc0646c4b777e65a01b4825755a46
shashi634 <shankar634@hotmail.com>
2019-11-10 15:51:01 +0530

    Questions  Page

--- questions/models.py
+++ questions/models.py
@@ -5,3 +5,28 @@ import uuid
 class Subject(models.Model):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     Name = models.CharField(max_length=100)
+
+class Questions(models.Model):
+    id = models.AutoField(primary_key=True)
+    PublicId = models.UUIDField(default=uuid.uuid4)
+    Question = models.TextField()
+    CreatedDate = models.DateTimeField()
+    CreatedBy = models.UUIDField(default=uuid.uuid4)
+    LastUpdatedDate = models.DateTimeField()
+    LastUpdatedBy = models.UUIDField(default=uuid.uuid4)
+    Subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+class Options(models.Model):
+    id = models.AutoField(primary_key=True)
+    PublicId = models.UUIDField(default=uuid.uuid4)
+    Question = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    OptionsA = models.TextField()
+    OptionsB = models.TextField()
+    OptionsC = models.TextField()
+    OptionsD = models.TextField()
+    CorrectiOption = models.TextField()
+    IsMultipleOption = models.BooleanField(default=False)
+    CreatedDate = models.DateTimeField()
+    CreatedBy = models.UUIDField(default=uuid.uuid4)
+    LastUpdatedDate = models.DateTimeField()
+    LastUpdatedBy = models.UUIDField(default=uuid.uuid4)
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Questions, Options
 # Create your views here.
 def getQuestions(request):
     return render(request,'Questions.html',{'result': 23})
+
+def addQuestions(request):
+    if  request.method == 'POST':
+        try:
+            organizationName = request.POST['organizationName']
+
+        except Exception as e:
+            messages.error(request, e)
+            return HttpResponseRedirect('/organization')
